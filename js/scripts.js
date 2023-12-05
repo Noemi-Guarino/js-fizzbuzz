3d0b506daa492d8231bcf10289c761f89b0d6d8d
Noemi-Guarino <noemi.guarino.1996@gmail.com>
2023-12-05 18:09:13 +0100

    edit

--- js/scripts.js
+++ js/scripts.js
@@ -1,17 +1,32 @@
+const target = document.getElementById('target');
+
 for (let i = 1; i < 101; i++){
 
+    let div = document.createElement("div");
+    div.classList.add("square");
+
     if (i % 3 == 0 && i % 5 == 0 ){
-    console.log("FizzBuzz");
+        console.log("FizzBuzz");
+        div.append("FizzBuzz");
+        div.classList.add("pink");
     }
     else if (i % 3 == 0){
-    console.log("Fizz");
+        console.log("Fizz");
+        div.append("Fizz");
+        div.classList.add("acquamarine");
     }   
     else if (i % 5 == 0){
-    console.log("Buzz");
+        console.log("Buzz");
+        div.append("Buzz");
+        div.classList.add("yellow");
     }    
     else {
         console.log(i);
+        div.append("i");
+        div.classList.add("blue");
     } 
     
+    target.append(div);
+
 }
 
